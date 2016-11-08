86b370cc798a2c6f47ada9a888a3a299d57eb0ee
Marta Kule <marta.kule@gmail.com>
2016-11-08 06:10:16 -0500

    Update rhymes.py

--- rhymes.py
+++ rhymes.py
@@ -1,17 +1,13 @@
 # Three strings of the text with blanks to fill in
-easy_questions =  "Twinkle, twinkle, little (1)_____,\nHow I wonder what you (2)_____!\nUp above the world so (3)_____,\nLike a diamond in the (4)_____."
-
-medium_questions = "Tweedledum and Tweedledee\nAgreed to have a (1)_____;\nFor Tweedledum said (2)_____\nHad spoiled his nice new rattle.\nJust then flew down a monstrous (3)_____,\nAs black as a tar-barrel;\nWhich frightened both the heroes so,\nThey quite forgot their (4)_____."
-
-hard_questions = "Tinker, (1)_____,\nSoldier, (2)_____,\nRich Man, (3)_____ Man,\nBeggar Man, (4)_____."
+questions = ["Twinkle, twinkle, little (1)_____,\nHow I wonder what you (2)_____!\nUp above the world so (3)_____,\nLike a diamond in the (4)_____.", "Tweedledum and Tweedledee\nAgreed to have a (1)_____;\nFor Tweedledum said (2)_____\nHad spoiled his nice new rattle.\nJust then flew down a monstrous (3)_____,\nAs black as a tar-barrel;\nWhich frightened both the heroes so,\nThey quite forgot their (4)_____.", "Tinker, (1)_____,\nSoldier, (2)_____,\nRich Man, (3)_____ Man,\nBeggar Man, (4)_____."]
 
 # Three lists of answers
-easy_answers=['star', 'are', 'high', 'sky']
-medium_answers=['battle', 'Tweedledee', 'crow', 'quarrel']
-hard_answers=['Tailor', 'Sailor', 'Poor', 'Thief']
+answers=[['star', 'are', 'high', 'sky'], ['battle', 'Tweedledee', 'crow', 'quarrel'], ['Tailor', 'Sailor', 'Poor', 'Thief']]
 
 # A list of blanks
-blank_list=["(1)_____","(2)_____","(3)_____","(4)_____"]
+blanks=["(1)_____","(2)_____","(3)_____","(4)_____"]
+
+levels=['easy','medium','hard']
 
 #The number of allowed wrong aswers per question
 wrong_limit=5
@@ -19,7 +15,7 @@ wrong_limit=5
 # This function prompts the user to pick a level and loads the appropriate level's data from above.
 def level_choice():
 	global level
-	choice = raw_input("Pick a level by typing easy, medium, or hard.\n")
+	choice = raw_input("Pick a level by typing easy, medium, or hard.\n").lower()
 	level=choice
 	if choice=="easy":
 		return start_game(easy_questions, easy_answers)
@@ -34,7 +30,7 @@ def level_choice():
 # It also contains the counter that controls the whole game.
 # And the counter for wrong answers because I can't figure it out.
 def start_game(ques,ans):
-	print "\n*****\n\nYou've picked level " + level + "! \nComplete the rhyme below one word at a time.\nYou will have 5 tries for each answer\nGood luck!\n\n*****\n"
+	print "\n*****\n\nYou've picked level " + level + "! \nComplete the rhyme below one word at a time.\nYou will have " + str(wrong_limit) + " tries for each question\nGood luck!\n\n*****\n"
 	global filled_rhyme
 	filled_rhyme=ques	
 	global counter
@@ -47,34 +43,35 @@ def start_game(ques,ans):
 # It also ends the game when the counter hits the last number.
 # Now it also handles the wrong answer, missing the counter per question and separate sentences.
 def game(filled_rhyme,ans,counter,chances):
-	while counter<len(blank_list):
+	while counter<len(blanks):
 		print "Now the rhyme reads like this:\n\n" + filled_rhyme
-		user_answer=raw_input("\n*****\n\nWhat word replaces"+blank_list[counter]+"?\n")
+		user_answer=raw_input("\n*****\n\nWhat word replaces"+blanks[counter]+"?\n\n>")
 		if answer_check (user_answer,ans[counter]) == True:
 			chances=wrong_limit #Following advice on the forum, I added this to reset wrong answers for the whole game.
-			filled_rhyme=filled_rhyme.replace((blank_list[counter]),ans[counter])
+			filled_rhyme=filled_rhyme.replace((blanks[counter]),ans[counter])
 		else:
 			chances=chances-1
 			if chances==0:
-				print "Game over.\nThanks for playing!"
+				print "*****\n\nGame over.\nThanks for playing!\n"
 				quit()
 			else:
 				wrong_answer_countdown(chances) #And this simple print function doesn't use any counters, so it doesn't reset.
 				return game(filled_rhyme,ans,counter,chances)
 		counter+=1
-	if counter==len(blank_list):
+	if counter==len(blanks):
 		print filled_rhyme
 		print "\n*****\n\nCongratulations! You completed the game.\nThanks for playing!"
 
+# This function prints a different sentence for 2 and 1 tries left.
 def wrong_answer_countdown(chances):
 	if chances > wrong_limit-(wrong_limit-2):
-		print "\nTry again!\nYou have " +str(chances) + " more tires!"
+		print "\n*****\n\nTry again! You have " +str(chances) + " more tires!\n"
 	if chances == wrong_limit-(wrong_limit-2):
-		print "\nThink before you answer!\nYou have only " +str(chances) + " more tires!"
+		print "\n*****\n\nGive it a think! You have only " +str(chances) + " more tires!\n"
 	if chances == wrong_limit-(wrong_limit-1):
-		print "\nMake this count!\nThis is your " +str(chances) + " last try!"
+		print "\n*****\n\nMake this count! This is your " +str(chances) + " last try!\n"
 		
-#This function takes the user's answer and checks it against the provided answers, ignoring case.
+# This function takes the user's answer and checks it against the provided answers, ignoring case.
 def answer_check (user_answer,ans):
 	if user_answer.lower()==ans.lower():
 		print "\nCorrect!\n"
