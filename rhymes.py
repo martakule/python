d4906f375e16c907b754c8f3a1d4276881e211be
Marta Kule <marta.kule@gmail.com>
2016-11-09 05:56:53 -0500

    Update rhymes.py

--- rhymes.py
+++ rhymes.py
@@ -17,47 +17,26 @@ wrong_limit=5
 safe_word="stop"
 
 # This function prompts the user to pick a level and loads the appropriate level's data from above.
-# It takes user input, transforms it into the index from the levels list, and keeps using that
-# number to pull the correct set of questions and answers to call in the start_game() function.
-# Before it calls the function, it welcomes the user to the selected level and gives the
-# number of allowed wrong answers and the word to terminate the game at any point.
+# It also sets the control counter and assigns the correct poem and number of allowed wrong answers.
 def level_choice():
-	choice = raw_input("Pick a level by typing easy, medium, or hard.\n")
+	choice = raw_input("Pick a level by typing easy, medium, or hard.\n>")
 	if choice.lower() not in levels:
 		print "\n*****\n\nInvalid input.\n"
 		return level_choice()
 	else:
 		choice=levels.index(choice)
 		print "\n*****\n\nYou've picked level " + levels[choice] + "! \nComplete the rhyme below one word at a time.\nYou will have " + str(wrong_limit) + " tries for each question\nIf you want to stop the game at any time, just type in " + safe_word + ".\nGood luck!\n\n*****\n"
-		return start_game (questions[choice],answers[choice])	
-
-# This function basically only stores values that cannot be stored in the next function, game().
-# This is because game() includes the case of wrong answer that call game() again.
-# So when these values aren't outside the function, they reset with every new call of game().
-# If you know a way to solve this problem without setting up global values, please show me.
-# Otherwise, if this is a valid solution, please tell me too. I'm a python noob.
-# Anyway, this function assigns the level's appropriate poem to value rhyme; starts a counter to
-# control the traversing through the questions and answers; and assigns the number of allowed
-# wrong anwers to value chances. Then it calls game().
-def start_game(ques,ans):
-	global rhyme
-	rhyme=ques	
-	global counter
-	counter=0
-	global chances
-	chances=wrong_limit
-	return game (rhyme,ans,counter,chances)
+		rhyme=questions[choice]	
+		counter=0
+		chances=wrong_limit
+		return game (rhyme,answers[choice],counter,chances)	
 	
-# This function takes the poem, counter, and number of allowed wrong answers assigned in start_game().
-# It also takes answers from the original list, through the functions above.
-# It first prints the poem woth blanks. It then asks for user's answer through input.
-# It calls function answer_check that takes that input, the right answer, and the safe word.
-# For a correct answer, and it replaces the blank with the right answer from the list (to maintain 
-# ccorrect ase/capitalization).
-# For a wrong answer, it subtracts one remaining allowed wrong answer and checks for their number.
-# If it's 0, it prints game over and quits. If it's more, it calls wrong_answer_countdown().
-# It then adds one to the controlling counter. If it equals the length of the blanks list, it completes
-# the game.
+# This function takes, correct answers and the poem, counter, and number of allowed wrong answers 
+# assigned in level_choice(). It prints the poem woth blanks, asks uses for answer.
+# Correct answer replaces the blank with the right answer from the list.
+# Wrong answer subtracts one remaining allowed wrong answer and checks for their number.
+# If it's 0, it prints game over and quits. The function then adds 1 to the controlling counter.
+# If it equals the length of the blanks list, it completes the game.
 def game(rhyme,ans,counter,chances):
 	while counter<len(blanks):
 		print "Now the rhyme reads like this:\n\n" + rhyme
@@ -79,27 +58,25 @@ def game(rhyme,ans,counter,chances):
 		print "\n*****\n\nCongratulations! You completed the game.\nThanks for playing!\n"
 
 # This function takes the number of remaining allowed wrong answers and prints a different sentence 
-# for 2 and 1 tries left. The total number of tries is a variable (the maximum can be anything),
-#so I set different sentences for only the last two.
+# for 2 and 1 tries left.
 def wrong_answer_countdown(chances):
-	if chances > wrong_limit-(wrong_limit-2):
-		print "\n*****\n\nTry again! You have " +str(chances) + " more tires!\n"
 	if chances == wrong_limit-(wrong_limit-2):
 		print "\n*****\n\nGive it a think! You have only " +str(chances) + " more tires!\n"
-	if chances == wrong_limit-(wrong_limit-1):
+	elif chances == wrong_limit-(wrong_limit-1):
 		print "\n*****\n\nMake this count! This is your " +str(chances) + " last try!\n"
+	else:
+		print "\n*****\n\nTry again! You have " +str(chances) + " more tires!\n"
 		
-# This function takes the user's answer and checks it against the provided answers, ignoring case.
+# This function takes the user's answer and checks it against the provided answers.
 # It also takes the safe word -- if the user's input equals it, it prints a farewell message and
-# terminates the game. If the answer is correct, it prints a message in this effect and returns
-# the True value to feed back to game(). Else, it returns False to trigger the wrong answer
-# action in game().
+# terminates the game. If the answer is correct, it prints a message and returns the True value.
+# Else, it returns False to trigger the wrong answer action in game().
 def answer_check (user_answer,ans,safe_word):
 	if user_answer.lower()==ans.lower():
-		print "\nCorrect!\n"
+		print "\n*****\n\nCorrect!\n"
 		return True 
-	if user_answer.lower()==safe_word.lower():
-		print "\nAll right! We're stopping here. Thanks for playing!\n"
+	elif user_answer.lower()==safe_word.lower():
+		print "\n*****\n\nAll right! We're stopping here. Thanks for playing!\n"
 		quit()
 	else:
 		return False 
